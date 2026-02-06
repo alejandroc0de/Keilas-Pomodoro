3ef63587e80d52662529e3dc7c51dfbd3b240884
Alejandro Carrillo <alejandro@Alejandros-MacBook-Air.local>
2026-02-06 18:42:14 -0500

    Add pomodoro layout

--- main.js
+++ main.js
@@ -48,3 +48,25 @@ let volumeNight = document.getElementById("volume-slider-night")
 volumeNight.addEventListener("change", function(e){
     nightSound.volume = e.currentTarget.value /100;
 })
+
+
+/// POMODORO LOGIC 
+
+let timmer = document.getElementById("timmer")
+const time25 = document.getElementById("25min")
+const time45 = document.getElementById("45min")
+const time5 = document.getElementById("5min")
+const time15 = document.getElementById("15min")
+
+time25.addEventListener("click", () => {
+    timmer.textContent = "25:00";
+})
+time15.addEventListener("click", () => {
+    timmer.textContent = "15:00";
+})
+time5.addEventListener("click", () => {
+    timmer.textContent = "05:00";
+})
+time45.addEventListener("click", () => {
+    timmer.textContent = "45:00";
+})
